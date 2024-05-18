8fd87ceb71cc578598e8c965c1b37b3c80490e1f
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-05-18 18:53:52 +0545

    chage random number functio

--- app/helpers.php
+++ app/helpers.php
@@ -1,8 +1,13 @@
 <?php
 
+
 if (!function_exists('generate_referral_number')) {
     function generate_referral_number()
     {
-        return str_pad(mt_rand(1, 999999999999), 12, '0', STR_PAD_LEFT);
+        do {
+            $referralNumber = str_pad(mt_rand(1, 999999999999), 12, '0', STR_PAD_LEFT);
+        } while (\App\Models\User::where('usercode', $referralNumber)->exists());
+
+        return $referralNumber;
     }
 }
